0ef7bf5543b772b1b5edebc7a078678d866e2250
Ubuntu <ubuntu@ip-172-31-44-88.ap-northeast-1.compute.internal>
2021-03-06 08:40:28 +0000

    modify linux command

--- linuxCommands/run.bash
+++ linuxCommands/run.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar &
-java -jar review-service/target/review-service-0.0.1-SNAPSHOT.jar &
-java -jar recommendation-service/target/recommendation-service-0.0.1-SNAPSHOT.jar &
-java -jar product-composite-service/target/product-composite-service-0.0.1-SNAPSHOT.jar &
+java -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar & > /dev/null
+java -jar review-service/target/review-service-0.0.1-SNAPSHOT.jar & > /dev/null
+java -jar recommendation-service/target/recommendation-service-0.0.1-SNAPSHOT.jar & > /dev/null
+java -jar product-composite-service/target/product-composite-service-0.0.1-SNAPSHOT.jar & > /dev/null
