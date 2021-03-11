56b3e398c5a7737a19f1954f71cde4f2ff4e0d24
Michael Ouyang <michaelouyang19971997@gmail.com>
2021-03-11 15:25:49 +0800

    update review

--- review-service/src/main/java/tw/ouyang/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/tw/ouyang/reviewservice/ReviewServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @ComponentScan("tw.ouyang")
+@SpringBootApplication
 public class ReviewServiceApplication {
 
 	public static void main(String[] args) {
