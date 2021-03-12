b7ab42ac2f7adb6f7c2cb3cee478d588b538c236
Michael Ouyang <michaelouyang19971997@gmail.com>
2021-03-12 13:21:19 +0800

    Add files via upload

---
+++ windowsCommands/run_scale.bat
@@ -0,0 +1,12 @@
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar --server.port=7001
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar --server.port=7002
+
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar review-service/target/review-service-0.0.1-SNAPSHOT.jar --server.port=7003
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar review-service/target/review-service-0.0.1-SNAPSHOT.jar --server.port=7004
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar review-service/target/review-service-0.0.1-SNAPSHOT.jar --server.port=7005
+
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar recommendation-service/target/recommendation-service-0.0.1-SNAPSHOT.jar  --server.port=7006
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar recommendation-service/target/recommendation-service-0.0.1-SNAPSHOT.jar  --server.port=7007
+
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar product-composite-service/target/product-composite-service-0.0.1-SNAPSHOT.jar
+start D:\MINE\software\jdk-11.0.10+9\bin\javaw -jar eureka-server/target/eureka-server-0.0.1-SNAPSHOT.jar
